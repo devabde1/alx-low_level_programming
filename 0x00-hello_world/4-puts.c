61cd456d95fba4d1489b65a6ae50060ebd50092e
devabde1 <domainame.help@gmail.com>
2023-09-07 17:27:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the first c programm i wrote in entire my life
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"); 
+return (0);
+}	
