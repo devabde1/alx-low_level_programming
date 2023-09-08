59e18d6f8de819eebb22620a5b7e6cb56b71f74c
devabde1 <domainame.help@gmail.com>
2023-09-07 17:48:29 -0700

    second one here

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the second c programm i wrote in entire my life
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
